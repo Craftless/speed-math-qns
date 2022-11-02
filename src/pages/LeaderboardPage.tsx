c5e7d50596ea7550e699b7fb5c752f197f913ea0
AV3_08 <66512356+AV306@users.noreply.github.com>
2022-11-02 12:07:55 +0800

    Update LeaderboardPage.tsx

--- src/pages/LeaderboardPage.tsx
+++ src/pages/LeaderboardPage.tsx
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
 import LeaderboardEntry from "../components/LeaderboardEntry";
+import LoadingSpinner from "../components/LoadingSpinner";
+import { projectFirestore } from "../firebase/config";
 import classes from "./LeaderboardPage.module.css";
 
 class PlayerRepresentation {
@@ -12,6 +14,8 @@ class PlayerRepresentation {
   }
 }
 
+// Unfortunately, I have to use regular arrays here
+// because states would "exceed the maximum update depth"
 var leaderboardArray: JSX.Element[];
 var players: PlayerRepresentation[];
 
@@ -19,16 +23,20 @@ function LeaderboardPage()
 {
   useEffect( () =>
   {
+    // 1. Empty the arrays
     leaderboardArray = [];
+    players = [];
+
     // 1. Fetch data from Firebase
+    //await projectFirestore.collection( "users" ).doc
 
     // 2. Construct array of PlayerRepresentations?
     players = [ new PlayerRepresentation( "a", 523 ), new PlayerRepresentation( "de", 444 ) ];
 
-    // 3. Sort by score???
+    // 3. Sort by score
     players.sort( (a, b) => { return b.score - a.score; } );
 
-    // 4. Add <LeaderboardEntry />s
+    // 4. Add <LeaderboardEntry /> elements
     for ( var i: number = 1; i <= players.length; i++ )
     {
       var player: PlayerRepresentation = players[i-1];
@@ -41,8 +49,13 @@ function LeaderboardPage()
       <h2 className={classes.leaderboardLabel}>Leaderboard</h2>
       <div className={classes.mainContainer}>
         <div className={classes.leaderboardContainer}>
-          { leaderboardArray }
+          {
+            leaderboardArray ? leaderboardArray :
+            // Can someone help me center this 
+            <LoadingSpinner width={20} height={20} borderWidth={4} />
+          }
         </div>
+        <p>If leaderboard fails to load, press "Leaderboard" on the title bar again :)</p>
       </div>
     </>
   );
