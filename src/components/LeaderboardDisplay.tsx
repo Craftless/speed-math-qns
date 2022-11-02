e54e24f585f35aee38dfdd0ab1c1d89d1709aa8e
Austin Zeng <65018226+AustinJason@users.noreply.github.com>
2022-11-02 02:35:06 +0000

    Edited leaderboard HTML and CSS

--- src/components/LeaderboardDisplay.tsx
+++ src/components/LeaderboardDisplay.tsx
@@ -3,12 +3,29 @@ import classes from "./LeaderboardDisplay.module.css";
 function LeaderboardDisplay()
 {
   return (
-    <div className={classes.leaderboardDisplay}>
-      <h2>Leaderboard</h2>
-        <ol className={classes.leaderboardList}>
-          // TODO
-        </ol>
+    <h2>Leaderboard</h2>
+
+<div id="container">
+    <div class="row">
+        <div class="Position">1</div><div class="name">Player1</div><div class="score">430</div>
     </div>
+    
+    <div class="row">
+        <div class="Position">2</div><div class="name">Player2</div><div class="score">580</div>
+    </div>
+    
+    <div class="row">
+         <div class="Position">3</div><div class="name">Player3</div><div class="score">310</div>
+    </div>
+    
+    <div class="row">
+         <div class="Position">4</div><div class="name">Player4</div><div class="score">640</div>
+    </div>
+    
+    <div class="row">
+         <div class="Position">5</div><div class="name">Player5</div><div class="score">495</div>
+    </div>
+</div>
   );
 }
 
