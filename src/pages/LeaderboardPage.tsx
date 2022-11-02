376f0ddcd884eb36faf0e316a0e961006c705a5b
AV3_08 <66512356+AV306@users.noreply.github.com>
2022-11-02 11:40:46 +0800

    buggy leaderboard

--- src/pages/LeaderboardPage.tsx
+++ src/pages/LeaderboardPage.tsx
@@ -1,11 +1,50 @@
-import LeaderboardDisplay from "../components/LeaderboardDisplay";
+import { useEffect } from "react";
+import LeaderboardEntry from "../components/LeaderboardEntry";
 import classes from "./LeaderboardPage.module.css";
 
-function LeaderboardPage() {
+class PlayerRepresentation {
+  name: string;
+  score: number;
+
+  constructor( name:string, score: number ) {
+    this.name = name;
+    this.score = score;
+  }
+}
+
+var leaderboardArray: JSX.Element[];
+var players: PlayerRepresentation[];
+
+function LeaderboardPage()
+{
+  useEffect( () =>
+  {
+    leaderboardArray = [];
+    // 1. Fetch data from Firebase
+
+    // 2. Construct array of PlayerRepresentations?
+    players = [ new PlayerRepresentation( "a", 523 ), new PlayerRepresentation( "de", 444 ) ];
+
+    // 3. Sort by score???
+    players.sort( (a, b) => { return b.score - a.score; } );
+
+    // 4. Add <LeaderboardEntry />s
+    for ( var i: number = 1; i <= players.length; i++ )
+    {
+      var player: PlayerRepresentation = players[i-1];
+      leaderboardArray.push( <LeaderboardEntry position={i} name={player.name} score={player.score} /> );
+    }
+  }, [players] );
+
   return (
-    <div className={classes.mainContainer}>
-      <LeaderboardDisplay />
-    </div>
+    <>
+      <h2 className={classes.leaderboardLabel}>Leaderboard</h2>
+      <div className={classes.mainContainer}>
+        <div className={classes.leaderboardContainer}>
+          { leaderboardArray }
+        </div>
+      </div>
+    </>
   );
 }
 
